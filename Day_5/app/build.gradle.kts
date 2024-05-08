ee9685e4d68852de43f27c5314589b1c6f4cbab1
wira07 <wiralodrasaputra07@gmail.com>
2024-05-08 08:56:17 +0700

    menambahkan font

--- Day_5/app/build.gradle.kts
+++ Day_5/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.wira_sukma_saputra.botanikacare"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.wira_sukma_saputra.botanikacare"
@@ -33,11 +33,14 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
---
+++ Day_5/app/src/main/java/com/wira_sukma_saputra/botanikacare/SignUpActivity.kt
@@ -0,0 +1,11 @@
+package com.wira_sukma_saputra.botanikacare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
