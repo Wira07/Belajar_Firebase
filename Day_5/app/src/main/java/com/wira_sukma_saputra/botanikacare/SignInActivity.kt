fc1b97ab68492839ef5fa890f53565e049626394
wira07 <wiralodrasaputra07@gmail.com>
2024-05-11 06:32:32 +0700

    update android bagian validasi google

--- Day_5/app/src/main/java/com/wira_sukma_saputra/botanikacare/SignInActivity.kt
+++ Day_5/app/src/main/java/com/wira_sukma_saputra/botanikacare/SignInActivity.kt
@@ -1,12 +1,19 @@
 package com.wira_sukma_saputra.botanikacare
 
+import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Patterns
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.wira_sukma_saputra.botanikacare.databinding.ActivitySignInBinding
@@ -14,11 +21,21 @@ import com.wira_sukma_saputra.botanikacare.databinding.ActivitySignInBinding
 class SignInActivity : BaseActivity() {
     private var binding: ActivitySignInBinding? = null
     private lateinit var auth: FirebaseAuth
+
+    private lateinit var googleSignInClient: GoogleSignInClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding?.root)
         auth = Firebase.auth
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.forgot))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+
 
         binding?.create?.setOnClickListener {
             startActivity(Intent(this,SignUpActivity::class.java))
@@ -31,6 +48,10 @@ class SignInActivity : BaseActivity() {
 
         binding?.SignIn2?.setOnClickListener {
             sinInUser()
+        }
+
+        binding?.signupButton?.setOnClickListener {
+            sinInWithGoogle()
         }
 
     }
@@ -58,6 +79,54 @@ class SignInActivity : BaseActivity() {
                 }
         }
     }
+
+    private fun sinInWithGoogle()
+    {
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
+    }
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+    {result->
+        if (result.resultCode == Activity.RESULT_OK)
+        {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            handleResults(task)
+        }
+    }
+
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful)
+        {
+            val account: GoogleSignInAccount? = task.result
+            if (account!=null) {
+                updateUI(account)
+            }
+        }
+        else
+        {
+            Toast.makeText(this,"Sign In Failed, try again.",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+        showProgressBar()
+        val  credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener{
+            if (it.isSuccessful)
+            {
+                startActivity(Intent(this,MainActivity::class.java))
+                finish()
+                hideProgressBar()
+            }
+            else
+            {
+                showToast(this,"Oops! Something went wrong")
+                hideProgressBar()
+            }
+        }
+    }
+
 
     private fun validateForm(email:String, password:String):Boolean
     {
