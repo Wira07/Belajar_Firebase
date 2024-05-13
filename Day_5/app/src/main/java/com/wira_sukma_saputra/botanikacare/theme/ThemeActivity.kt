51e80d860e7f05ea992a4266989796157afa1f90
wira07 <wiralodrasaputra07@gmail.com>
2024-05-13 17:35:32 +0700

    Menambahkan folder Theme

---
+++ Day_5/app/src/main/java/com/wira_sukma_saputra/botanikacare/theme/ThemeActivity.kt
@@ -0,0 +1,36 @@
+package com.wira_sukma_saputra.botanikacare.theme
+
+import android.os.Bundle
+import android.widget.CompoundButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.switchmaterial.SwitchMaterial
+import com.wira_sukma_saputra.botanikacare.R
+
+class ThemeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_theme)
+
+        val switchTheme = findViewById<SwitchMaterial>(R.id.switch_theme)
+
+        val pref = SettingPreferences.getInstance(application.dataStore)
+        val mainViewModel = ViewModelProvider(this, ViewModelFactory(pref)).get(
+            MainViewModel::class.java
+        )
+        mainViewModel.getThemeSettings().observe(this) { isDarkModeActive: Boolean ->
+            if (isDarkModeActive) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                switchTheme.isChecked = true
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                switchTheme.isChecked = false
+            }
+        }
+
+        switchTheme.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
+            mainViewModel.saveThemeSetting(isChecked)
+        }
+    }
+}
